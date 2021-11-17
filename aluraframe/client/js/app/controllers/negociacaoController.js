37b302a00fdcdeb03ba28d8782ccea6f9d492db3
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-11-16 21:31:02 -0300

    Javascript: Arrow Function, spread, map

--- aluraframe/client/js/app/controllers/negociacaoController.js
+++ aluraframe/client/js/app/controllers/negociacaoController.js
@@ -1,16 +1,28 @@
 class NegociacaoController {
 
+    constructor(){
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event){
 
         event.preventDefault();
         
-        let $ = document.querySelector.bind(document);
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice %2)
+        );  
         
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value); 
+        let negociacao = new Negociacao(
+            data, 
+            this._inputQuantidade.value,
+            this._inputValor.value    
+        );
+        console.log(negociacao); 
     }
 } 

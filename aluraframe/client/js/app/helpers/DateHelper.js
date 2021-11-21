50ad3505bfff587c04dc28eace1241c3426b2396
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-11-21 11:37:09 -0300

    Javascript: apenas um comentário

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -13,7 +13,7 @@ class DateHelper{
 
         if (!/\d{4}-\d{2}-\d{2}/.test(texto)){
             throw new Error('Deve estar no formato aaaa-mm-dd');
-        }
+        } /* método fast-fail */
         return new Date(...texto.split('-').map((item, indice) => item - indice %2)) ;
 
     }
